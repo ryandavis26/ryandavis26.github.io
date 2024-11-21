a714fd399edd358fea7368c8cd583fbc9ef79dd9
Ryan Davis <ryandavisim@gmail.com>
2024-11-20 19:38:20 -0500

    changed mobile navbar and js selector for mobile nav bar, fixed typo in python project

--- main.js
+++ main.js
@@ -5,21 +5,19 @@ const project1Btn = document.querySelector('#project-1-btn')
 const project2Btn = document.querySelector('#project-2-btn')
 const project3Btn = document.querySelector('#project-3-btn')
 
-
-
-menuIcon.onClick = () => {
+menuIcon.addEventListener('click', () => {
   navLinks.classList.toggle('active'); 
-}
 
+});
 visitButton.addEventListener('click', () => {
     window.location.href = 'https://github.com/ryandavis26'
-})
+});
 project1Btn.addEventListener('click', () => {
     window.location.href = 'https://www.youtube.com/watch?v=iushWXrmKXc&ab_channel=RyanDavis'
-})
+});
 project2Btn.addEventListener('click', () => {
     window.location.href = 'https://www.youtube.com/embed/y6dVWXbnfBU?si=fS75TWleyO8MxKAu'
-})
+});
 project3Btn.addEventListener('click', () => {
     window.location.href = 'https://www.youtube.com/embed/yBOC6zRX7wk?si=nLInOT1G9shnWidu'
-})
+});
